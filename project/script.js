df8fa167fe48dfaeb80f3cf28178475cce0332a1
gibr098 <sirico.1810153@studenti.uniroma1.it>
2022-07-01 23:58:08 +0200

    models chages

--- project/script.js
+++ project/script.js
@@ -1,17 +1,40 @@
 import * as THREE from 'three';
+import * as MODELS from './mscript.js';
+import { TrackballControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/TrackballControls.js'
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js'
 
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
 
+
+
   const fov = 75;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  //camera.position.set(0,0,0);
+  //const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  //camera.position.set(0,0,7);
   //camera.position.z = 3;
-  camera.position.z = 6;
+  //camera.position.z = 6;
+  function makeCamera(fov = 40) {
+    const aspect = 2;  // the canvas default
+    const zNear = 0.1;
+    const zFar = 1000;
+    return new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
+  }
+  const camera = makeCamera();
+  camera.position.set(0, 90, -30)
+  camera.lookAt(0, 0, 0);
+
+const controls = new TrackballControls(camera, renderer.domElement );
+controls.rotateSpeed = 2.0;
+controls.zoomSpeed = 1.5;
+controls.panSpeed = 0.8;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x92FFF8);
@@ -20,7 +43,7 @@ function main() {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    light.position.set(10, 20, 4);
     scene.add(light);
   }
 
@@ -57,9 +80,9 @@ function main() {
     //makeInstance(geometry, 0xaa8844,  2, 1.5, -1 ),
   ];
 
-
+//Background
 //const geometryc = new THREE.CylinderGeometry( 2.5, 2.5, canvas.width*1/100, 32 );
-const geometryc = new THREE.CylinderGeometry( 2.5, 2.5, 6, 32 );
+const geometryc = new THREE.CylinderGeometry( 25, 25, 100, 32 );
 const materialc = new THREE.MeshPhongMaterial( {color: 0x1da2d8} );
 const cylinder = new THREE.Mesh( geometryc, materialc );
 //scene.add( cylinder );
@@ -81,7 +104,14 @@ group.add(cylinder);
 group.add(cubeA);
 group.add(cubeB)
 
-scene.add(group);
+//scene.add(group);
+/*
+const plane=PLANE.createPlane1();
+plane.position.set(0,0,0);
+plane.scale.set(0.5,0.5,0.5);
+*/
+var plane=new MODELS.Plane2();
+scene.add(plane.mesh);
 
 
   function resizeRendererToDisplaySize(renderer) {
@@ -94,6 +124,34 @@ scene.add(group);
     }
     return needResize;
   }
+  function setupKeyControls() {
+    document.addEventListener("keydown",function(event) {
+      switch (event.keyCode) {
+        case 37:
+          plane.mesh.position.x+=0.1;
+        break;
+        case 38:
+          plane.mesh.position.z+=0.1;
+        break;
+        case 39:
+          plane.mesh.position.x-=0.1;
+        break;
+        case 40:
+          plane.mesh.position.z-=0.1;
+        break;
+      }
+    });
+  }
+
+/*
+const controlPlane = new OrbitControls( plane, renderer.domElement );
+controlPlane.keys = {
+	LEFT: 'ArrowLeft', //left arrow
+	UP: 'ArrowUp', // up arrow
+	RIGHT: 'ArrowRight', // right arrow
+	BOTTOM: 'ArrowDown' // down arrow
+}*/
+
 
   function render(time) {
     time *= 0.001;  // convert time to seconds
@@ -110,16 +168,24 @@ scene.add(group);
       cube.rotation.y = rot;
     });
 
+    setupKeyControls();
+    //updatePlane();
     //cylinder.rotation.x = time;
+    //plane.rotation.z=2*time;
+    
     group.rotation.x=0.2*time;
 
-    
 
+    controls.update();
+    plane.propeller.rotation.z=20*time;
+    
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
+
+
   //console.log(canvas.clientWidth);
   //scene.add(cube);
 
@@ -137,6 +203,7 @@ scene.add(group);
   }
   requestAnimationFrame(render);
   */
+
 }
 
 main();

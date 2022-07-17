37dd14508b24ae4eb29dacf97035406bf47ef70c
gibr098 <sirico.1810153@studenti.uniroma1.it>
2022-07-17 18:14:41 +0200

    changes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ import * as MODELS from './mscript.js';
 //import * as FLOOR  from './script1.js';
 import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js'
 //import * as FUNCTIONS from './models.js';
-import { TrackballControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/TrackballControls.js'
-import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js'
 import { Water } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/objects/Water.js'
 import { randomPosition, Pause } from './functions.js';
 
@@ -25,14 +23,6 @@ const renderer = new THREE.WebGLRenderer({canvas});
 
 
   
-  const fov = 75;
-  const aspect = sizes.width / sizes.height;  // the canvas default
-  const near = 0.1;
-  const far = 5;
-  //const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  //camera.position.set(0,0,7);
-  //camera.position.z = 3;
-  //camera.position.z = 6;
   function makeCamera(fov = 40) {
     const aspect = sizes.width / sizes.height;  // the canvas default
     const zNear = 0.1;
@@ -54,62 +44,20 @@ const renderer = new THREE.WebGLRenderer({canvas});
   renderer.gammaOuput = true
   renderer.render(scene, camera)
 
-  const controls = new TrackballControls(camera, renderer.domElement );
-  controls.rotateSpeed = 2.0;
-  controls.zoomSpeed = 1.5;
-  controls.panSpeed = 0.8;
-  controls.noZoom = false;
-  controls.noPan = false;
-  controls.staticMoving = true;
-  controls.dynamicDampingFactor = 0.3;
-  {
+
+{
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-20, 20,20);
     scene.add(light);
-  }
+}
 
-  /*const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-  const boxWidth1 = 0.5;
-  const boxHeight1 = 1;
-  const boxDepth1 = 0.3;
-  const geometry1 = new THREE.BoxGeometry(boxWidth1, boxHeight1, boxDepth1);
-
-  //const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
-  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
-
-  const cube = new THREE.Mesh(geometry, material);
-  function makeInstance(geometry, color, x, y, z) {
-    const material = new THREE.MeshPhongMaterial({color});
-
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    cube.position.x = x;
-    cube.position.y = y;
-    cube.position.z = z;
-
-    return cube;
-  }
-
-  const cubes = [
-    //makeInstance(geometry, 0x44aa88,  0, -1, -2),
-    //makeInstance(geometry1, 0x8844aa, -4, 0, -2),
-    //makeInstance(geometry, 0xaa8844,  2, 1.5, -1 ),
-  ];
-*/
-//Background
-//const geometryc = new THREE.CylinderGeometry( 2.5, 2.5, canvas.width*1/100, 32 );
 
 
   const loader = new GLTFLoader()
   loader.load( 'assets/island.glb', function ( glb ) {
-      
+
       //const root = glb.scene
       myObj = glb.scene
       myObj.position.z +=80
@@ -121,8 +69,8 @@ const renderer = new THREE.WebGLRenderer({canvas});
 
     const load = new GLTFLoader()
     load.load( 'assets/mill.glb', function ( glb ) {
-        
-        //const root = glb.scene
+
+
         mill = glb.scene
         mill.position.z +=150
         mill.position.x -=40
@@ -135,7 +83,7 @@ const renderer = new THREE.WebGLRenderer({canvas});
       const loaderIsl = new GLTFLoader()
   loaderIsl.load( 'assets/shark.glb', function ( glb ) {
       console.log(glb)
-      //const root = glb.scene
+
       shark = glb.scene;
       shark.position.z +=50;
       shark.scale.set(10,10,10);
@@ -145,8 +93,8 @@ const renderer = new THREE.WebGLRenderer({canvas});
 
       scene.add(shark);
     })
-  
-  
+
+
     var waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
 
 var water = new Water(
@@ -170,34 +118,7 @@ water.rotation.x = - Math.PI / 2;
 water.position.y -= 70;
 scene.add(water)
 
-/*const geometryc = new THREE.CylinderGeometry( 25, 25, 100, 32 );
-const materialc = new THREE.MeshPhongMaterial( {color: 0x1da2d8} );
-const cylinder = new THREE.Mesh( geometryc, materialc );
-//scene.add( cylinder );
-cylinder.position.set(0, 0, 0);
-cylinder.rotation.z=Math.PI/2;
 
-const geometrycubeA = new THREE.BoxGeometry( 0.05, 0.05, 0.05 );
-const geometrycubeB = new THREE.BoxGeometry( 0.5, 0.5, 0.05 );
-const materialcube = new THREE.MeshBasicMaterial( {color: 0xC2B280} );
-
-const cubeA = new THREE.Mesh( geometrycubeA, materialcube );
-cubeA.position.set( 0, 0, 2.5 );
-
-const cubeB = new THREE.Mesh( geometrycubeB, materialcube );
-cubeB.position.set( 1, 0, 2.5 );
-
-const group = new THREE.Group();
-group.add(cylinder);
-group.add(cubeA);
-group.add(cubeB)
-
-//scene.add(group);
-/*
-const plane=PLANE.createPlane1();
-plane.position.set(0,0,0);
-plane.scale.set(0.5,0.5,0.5);
-*/
 
 
 var heart1= new MODELS.Heart();
@@ -221,8 +142,6 @@ var enemies=[];
 var enemy;
 
 
-
-//var selectedplane=planeselection();
 var plane;
 var selectedplane=localStorage.getItem("plane");
 
@@ -242,8 +161,8 @@ plane.playerLives=3;
 
 var plane1=new MODELS.Plane2();
 plane1.mesh.scale.set(0.5, 0.5, 0.5);
-//scene.add(plane1.mesh);
-//plane1.mesh.position.set(-25,0,40);
+scene.add(plane1.mesh);
+plane1.mesh.position.set(-25,0,40);
 
 var plane2=new MODELS.Plane2();
 plane2.mesh.scale.set(0.5, 0.5, 0.5);
@@ -256,11 +175,14 @@ plane3.mesh.scale.set(0.5, 0.5, 0.5);
 //plane3.mesh.position.set(0,0,15);
 
 
-
-
 plane1.mesh.rotation.y=Math.PI;
 plane2.mesh.rotation.y=Math.PI;
 plane3.mesh.rotation.y=Math.PI;
+
+enemies.push(plane1);
+//enemies.push(plane2);
+//enemies.push(plane3);
+
 
 
 
@@ -282,34 +204,9 @@ screen game area (x,z)
 //generatePlane();
 
 
-enemies.push(plane1);
-//enemies.push(plane2);
-//enemies.push(plane3);
 
 
 
-
-/*function resizeRendererToDisplaySize(renderer) {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-    renderer.setSize(width, height, false);
-  }
-  return needResize;
-}
-*/
-
-
-//Mouse control
-/*
-  document.addEventListener("mousemove", mouseMoveHandler);
-  function mouseMoveHandler(e) {
-    plane.mesh.position.x = -(e.pageX - canvas.offsetLeft);
-    plane.mesh.position.z = -(e.pageY - canvas.offsetTop);
-  }
-*/
 
 
 
@@ -428,55 +325,6 @@ function generateEnemyBullet(enemy){
   enemy.L1bullets.push(bulletl1E);
 }
 
-/*
-var maxpointnumber=7;
-function generatePlane(){
-  enemy=new MODELS.Plane2();
-  //enemy.mesh.position.set(randomPosition(-40,40) , 0, randomPosition(0, 60));
-  var p = enemies_positions[ randomPosition(0, maxpointnumber)];
-  if(!p.taken){
-    enemy.mesh.position.x=p.x;
-    enemy.mesh.position.y=p.y;
-    enemy.mesh.position.z=p.z;
-    p.taken=true;
-  enemy.positionPoint=p;
-  enemies_positions.pop(p);
-  maxpointnumber-=1;
-  enemy.mesh.scale.set(0.5, 0.5, 0.5)
-  enemy.mesh.rotation.y=Math.PI;
-  scene.add( enemy.mesh );
-  enemies.push(enemy);
-  console.log(enemies);
-  enemies_number+=1;
-  console.log(enemy.positionPoint);
-  console.log(enemies_positions);
-  }
-}
-
-
-function movePlane(enemy){
-  if(enemy.mesh.position.x < 50.0 && flag==true){
-    enemy.mesh.position.x+=0.5;
-    if(enemy.mesh.position.x>=50.0){ flag=false;}
-  }else if(enemy.mesh.position.x > -50 && flag==false){
-    enemy.mesh.position.x-=0.5;
-    if(enemy.mesh.position.x<=-50){flag=true;}
-  }
-}
-
-var flag1=true;
-function PlaneMovement(pl,angle){
-  if(pl.mesh.rotation.z < angle && flag1==true){
-    pl.mesh.rotation.z+=0.02;
-    /*
-    if(pl.mesh.rotation.z>=angle){ flag1=false;}
-  }else if(pl.mesh.rotation.z > -angle && flag1==false){
-    pl.mesh.rotation.z-=0.02;
-    if(pl.mesh.rotation.z<=-angle){flag1=true;}
-  }
-}
-
-*/
 
 function generatePlane(){
   enemyId++;
@@ -489,25 +337,12 @@ function generatePlane(){
   //enemy.mesh.position.set(randomPosition(-40,40) , 0, randomPosition(0, 60));
   enemy.mesh.position.set(randomPosition(-70,70) , 0, randomPosition(100, 200));
 
-  /*
-  var p = enemies_positions[ randomPosition(0, maxpointnumber)];
-  if(!p.taken){
-    enemy.mesh.position.x=p.x;
-    enemy.mesh.position.y=p.y;
-    enemy.mesh.position.z=p.z;
-    p.taken=true;
-  enemy.positionPoint=p;
-  enemies_positions.pop(p);
-  maxpointnumber-=1;
-  */
   enemy.mesh.scale.set(0.5, 0.5, 0.5)
   enemy.mesh.rotation.y=Math.PI;
   scene.add( enemy.mesh );
   enemies.push(enemy);
   //setInterval(function() {generateEnemyBullet(enemies[enemies.length-1])}, 2000);
   console.log(enemies);
-  //console.log(enemy.positionPoint);
-  //console.log(enemies_positions);
   var a = enemies[enemyId]
   setInterval(function() {generateEnemyBullet(a)}, 2000);
   
@@ -520,10 +355,7 @@ function collision(bullet, plane){
         (bullet.mesh.position.z<=plane.mesh.position.z+10 && bullet.mesh.position.z>=plane.mesh.position.z-10) &&
         (bullet.mesh.position.y<=plane.mesh.position.y+10 && bullet.mesh.position.y>=plane.mesh.position.y-10);
   if(c){
-    //console.log(c,"Collision");
     plane.hit=true;
-    //plane.positionPoint.taken=false;
-
     bullet.mesh.position.set(0,-10,0);
     scene.remove(bullet.mesh);
   }else{
@@ -531,7 +363,6 @@ function collision(bullet, plane){
       bullet.mesh.position.set(0,-100,0);
       scene.remove(bullet.mesh);
     }
-    //console.log(c,"NO collision");
   }
 }
 
@@ -620,7 +451,7 @@ var t = 3500; // Timer
 
 
 
-
+console.log(plane.playerLives);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -645,25 +476,7 @@ const angle=Math.PI/6;
 
 function render() {
   if(!pPressed){
-  
-  //time *= 0.001;  // convert time to seconds
- /* if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-  }
-*/
 
-  /*cubes.forEach((cube, ndx) => {
-    const speed = 1 + ndx * .1;
-    const rot = time * speed;
-    cube.rotation.x = rot;
-    cube.rotation.y = rot;
-  });
-*/
-  //cylinder.rotation.x = time;
-  //plane.rotation.z=2*time;
-  
   const vel=1.0;
   if(rightPressed && plane.mesh.position.x>-90) {
     plane.mesh.position.x -= vel;
@@ -715,7 +528,6 @@ function render() {
 
   var bullet_velocity=2;
   if (spacebarPressed){
-    //bullet.shoot(5);
     R1bullets.forEach(b => {
       b.mesh.position.z+=bullet_velocity;
       enemies.forEach(p => {
@@ -730,37 +542,14 @@ function render() {
       });
     });
   }
-  
-  //movePlane(plane1);
-  //movePlane(plane2);
-  //plane.mesh.rotation.x=Math.PI/6;
 
- 
-  
-
-  controls.update();
-
- /* enemies.forEach(p => {
-    p.propeller.rotation.z=20*time;
-    p.destroy();
-    if(p.mesh.position.y<=-100){
-      scene.remove(p.mesh);
-    }
-    //movePlane(p);
-  });
-  */
   water.material.uniforms[ 'time' ].value += 0.5 / 60.0;
-  //plane.propeller.rotation.z=20*time;
-  
 
-  //renderer.render(scene, camera);
-
-  //requestAnimationFrame(render);
    }
 }
 
 function animate(){
-
+console.log(pPressed);
     requestAnimationFrame(animate)
     renderer.render(scene,camera)
     const time = performance.now() * 0.001;
@@ -776,7 +565,6 @@ function animate(){
     })
     if(!pPressed){
     enemies.forEach(p => {
-      //p.propeller.rotation.z=20*time;
       p.movePlanePattern2(0.5,0.3);
       p.shoot();
       p.destroy();
@@ -823,16 +611,11 @@ function animate(){
       else{
         mill.position.z= 120;
       }
-    
+
     render();
     }
-  
-    
-   
-    
+
 }
 
-
-//main()
 animate()
 
